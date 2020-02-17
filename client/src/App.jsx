381302ba1ffe2c0359e8b76f87e7f4a399342010
dlovett712 <dlovett@rbi.com>
2020-02-17 15:08:55 -0500

    file changes into app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,39 @@
-import React from 'react';
-import Home from './components/Home'
-import Footer from './components/Footer'
-import './App.css';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import contact from './components/Contact';
+import Home from './components/Home';
 
-class App extends React.Component {
-  state = { serverMessage: '' };
-
-  componentDidMount() {
-    fetch('/api/demo')
-      .then(response => response.json())
-      .then(data => this.setState({ serverMessage: data.message }));
-  }
-
-   render() {
-    return (
-      <Home></Home>
-    );
-  }
-
-
-  render() {
-    return (
-      <Header></Header>
-    );
-  }
-
-  render() {
-    return (
-      <Footer></Footer>
-    );
-  }
+function App() {
+  return (
+    <Fragment>
+      <Router>
+        <div className="Home">
+          <nav class="locations">
+            <ul>
+              <li>
+                <Link to="/">--------</Link>
+              </li>
+              <li>
+                <Link to="/myeats">Myeats</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/Myeats" />
+          <Route path="/About" />
+          <Route exact path="/contact" component={contact} />
+        </Switch>
+        '
+      </Router>
+    </Fragment>
+  );
 }
-
 export default App;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,80 +1,11 @@
-import React, {Fragment} from 'react';
-import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
-import Carousel from 'react-bootstrap/Carousel'
+import React from 'react';
 
-function Home() {
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
 
-    return (
-      <Fragment>
-        <Router>
-          <div className="Home">
-            <nav class="locations">
-            <ul>
-              <li>
-                <Link to="/">--------</Link>
-              </li>
-              <li>
-                <Link to="/myeats">Myeats</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/contacts">Contact</Link>
-              </li>
-            </ul>
-        </nav>
-        </div>
-        <Switch>
-            <Route path="/"/>
-            <Route path="/Myeats"/>
-            <Route path="/About"/>
-            <Route path="/Contact"/>
-        </Switch>
-      </Router>
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="http://placekitten.com/g/200/300"
-            src=""
-            alt=""
-          />
-          <Carousel.Caption>
-            <h3></h3>
-            <p></p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        
-        
-        <Carousel.Item>
-          <img
-            className="http://placekitten.com/g/200/300"
-            src=""
-            alt=""
-          />
-        <Carousel.Caption>
-            <h3></h3>
-            <p></p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          
-          <img
-            className="http://placekitten.com/g/200/300"
-            src=""
-            alt=""
-          />
-          <Carousel.Caption>
-            <h3></h3>
-            <p></p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    
-      <button type="button">Lets Eat</button>
-    </Fragment>
-  )
-
-}  
-
-export default Home
+export default Home;
