b4ed41858ce8ccb210655e56a9b6ddd5ebfcd3d0
lavonnyka <lavonnyka@gmail.com>
2020-02-17 15:01:28 -0500

    server done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import './App.css';
+import "./"
+import "./components/MyEats"
+import Resturant from './components/MyEats';
 
 class App extends React.Component {
   state = { serverMessage: '' };
 
   componentDidMount() {
-    fetch('/api/demo')
+    fetch('/client/src/components/MyEats.js')
       .then(response => response.json())
       .then(data => this.setState({ serverMessage: data.message }));
   }
 
   render() {
     return (
-      <div id="demo">
-        <h1>Hello from client/src/App.js</h1>
-        <h1>{this.state.serverMessage}</h1>
-      </div>
+      <Resturant/>
+
+      // <div id="demo">
+      //   <h1>Hello from client/src/App.js</h1>
+      //   <h1>{this.state.serverMessage}</h1>
+      // </div>
     );
   }
 }
