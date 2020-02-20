ab1a307027099d4645e6103c52e25505934aa7d9
reidjamile <reidjamile@gmail.com>
2020-02-19 19:40:44 -0500

    about page final styles

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -7,12 +7,14 @@ import lava from "./pics/lava.jpeg";
 import sonia from "./pics/sonia.jpeg";
 import dean from "./pics/dean.jpeg";
 import atopbg from "./pics/atop.jpg";
-import logopl from "./pics/logo-placeholder.png";
-import forkpic from "./pics/fork.png";
-import knifepic from "./pics/knife.png";
-import scrollpic from "./pics/scroll.png";
+import logopl from "./pics/food-pictop1.png";
+import forkpic from "./pics/forkw.png";
+import knifepic from "./pics/knifew.png";
+import scrollpic from "./pics/scrolls.png";
 import baconl from "./pics/baconl.png";
 import baconr from "./pics/baconr.png";
+import githubPic from "./pics/github-clean100.png"
+import linkedinPic from "./pics/linkedin-clean100.png"
 
 export default function About() {
   let element = document.querySelector("#root");
@@ -31,7 +33,7 @@ export default function About() {
             <img src={forkpic} alt=" " />
           </span>
           <span id="logo-place">
-            <img src={logopl} alt=" " />
+            <img id="food-pic" src={logopl} alt=" " />
           </span>
           <span id="knife">
             <img src={knifepic} alt=" " />
@@ -69,12 +71,15 @@ export default function About() {
             <div id="about-ppg">
                 <h3 id="about-htext">About WynEats</h3>
                 <p id="about-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-                    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur.
+                Finding a nice restaurant here is a pain in the ass! When you’re confined 
+                to a small area like Wynwood, it can become a difficult task to find some 
+                food that ACTUALLY tastes good without spending your life savings! You would 
+                think that in a place with so much art, you could find a beautiful plate. 
+                Over 600 restaurants in Wynwood with the 50 best options displayed for you 
+                at the click of a button. WynEats is transforming how people experience 
+                Wynwood by giving anyone access to the best culinary options in the area. 
+                Our revolutionary platform connects customers with local businesses that 
+                can satisfy all of their tastebuds.
                 </p>
             </div>
         </div>
@@ -93,9 +98,29 @@ export default function About() {
           <span id="joe-about" class="member-about">
           <h4>Joe</h4>
             <p>
-            "The secret of getting ahead is getting started" - Mark Twain
+            "It's not a bug. it's a feature" - Unknown
             </p>
           </span>
+          <div> 
+              <span class="github">
+                <a href="https://github.com/sortinousn" target="_blank"><img
+                class="github-pic"
+                src={githubPic}
+                alt="github"
+                height="20"
+                width="auto"
+                /></a>
+              </span>
+              <span class="linkdin"> 
+              <a href="https://www.linkedin.com/in/josephsortino/" target="_blank"><img
+                class="linkedin-pic"
+                src={linkedinPic}
+                alt="linkedin"
+                height="20"
+                width="auto"
+                /></a>
+              </span>
+          </div>
         </div>
         <div class="member-pics">
           <span id="jamile-pic">
@@ -110,9 +135,29 @@ export default function About() {
           <span id="jamile-about" class="member-about">
           <h4>Jamile</h4>
             <p>
-            "The secret of getting ahead is getting started" - Mark Twain
+            "For the benefit of the flower, we water the thorns too" - Egyptian Proverb
             </p>
           </span>
+          <div> 
+              <span class="github">
+                <a href="https://github.com/reidjamile" target="_blank"><img
+                class="github-pic"
+                src={githubPic}
+                alt="github"
+                height="20"
+                width="auto"
+                /></a>
+              </span>
+              <span class="linkdin"> 
+              <a href="https://www.linkedin.com/in/jamile-reid-1b075a99/" target="_blank"><img
+                class="linkedin-pic"
+                src={linkedinPic}
+                alt="linkedin"
+                height="20"
+                width="auto"
+                /></a>
+              </span>
+          </div>
         </div>
         <div class="member-pics">
           <span id="lava-pic">
@@ -127,9 +172,30 @@ export default function About() {
           <span id="lava-about" class="member-about">
           <h4>Lava</h4>
             <p>
-            "The secret of getting ahead is getting started" - Mark Twain
+            "If you don't like something, change it. If you can't change it, change your 
+            attitude." -Maya Angelou
             </p>
           </span>
+          <div> 
+              <span class="github">
+                <a href="https://github.com/lavonnyka" target="_blank"><img
+                class="github-pic"
+                src={githubPic}
+                alt="github"
+                height="20"
+                width="auto"
+                /></a>
+              </span>
+              <span class="linkdin"> 
+              <a href="https://www.linkedin.com/in/lavonnyka-howell-20906316b" target="_blank"><img
+                class="linkedin-pic"
+                src={linkedinPic}
+                alt="linkedin"
+                height="20"
+                width="auto"
+                /></a>
+              </span>
+          </div>
         </div>
         <div class="member-pics">
           <span id="sonia-pic">
@@ -147,6 +213,26 @@ export default function About() {
             "Permanent messages taught by temporary people. " - Unknown
             </p>
           </span>
+          <div> 
+              <span class="github">
+                <a href="https://github.com/stremysonia" target="_blank"><img
+                class="github-pic"
+                src={githubPic}
+                alt="github"
+                height="20"
+                width="auto"
+                /></a>
+              </span>
+              <span class="linkdin"> 
+              <a href="http://www.linkedin.com/in/sonia-st-remy" target="_blank"><img
+                class="linkedin-pic"
+                src={linkedinPic}
+                alt="linkedin"
+                height="20"
+                width="auto"
+                /></a>
+              </span>
+          </div>
         </div>
         <div class="member-pics">
           <span id="dean-pic">
@@ -164,6 +250,26 @@ export default function About() {
             "The secret of getting ahead is getting started" - Mark Twain
             </p>
           </span>
+          <div> 
+              <span class="github">
+                <a href="https://github.com/dlovett712" target="_blank"><img
+                class="github-pic"
+                src={githubPic}
+                alt="github"
+                height="20"
+                width="auto"
+                /></a>
+              </span>
+              <span class="linkdin"> 
+              <a href="https://www.linkedin.com/in/dean-lovett/" target="_blank"><img
+                class="linkedin-pic"
+                src={linkedinPic}
+                alt="linkedin"
+                height="20"
+                width="auto"
+                /></a>
+              </span>
+          </div>
         </div>
       </div>
     </div>
